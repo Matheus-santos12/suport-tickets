3f729d33f5b7239d7b79dbccb813e7d0161cd985
Matheus Silva <rematheussilva@gmail.com>
2026-04-04 20:41:13 -0500

    Atualizando o ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,6 +1,12 @@
 export function update({ request, response, database }) {
   const { id } = request.params;
-  const { equipament, description } = request.body;
+  const { equipment, description } = request.body;
+
+  database.update("tickets", id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+  });
 
   return response.end(description);
 }
